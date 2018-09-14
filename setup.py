e5965b08782ba7cfc9abf65de0ae3855f8d672fa
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-09-14 13:48:46 -0400

    Added more keywords and strings classifiers items and updated others items for this package

--- setup.py
+++ setup.py
@@ -1,25 +1,42 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3'
+version = '0.3.1'
 
 setup(name='collective.ptg.quicksand',
       version=version,
-      description="Sheet gallery for plonetruegallery",
+      description="Quicksand Gallery Display Type extension for PloneTrueGallery package",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='plonetruegallery gallery plone addon',
+      keywords='web zope plone cms plonetruegallery gallery quicksand addon',
       author='Espen Moe-Nilssen',
       author_email='espen@medialog.no',
+      maintainer='Leonardo Caballero',
+      maintainer_email='leonardocaballero@gmail.com',
       url='https://github.com/collective/collective.ptg.quicksand',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
